818311769a19edce90f5d314c0eedd65543f6c98
MeetButani27 <20cs010@charusat.edu.in>
2023-05-20 08:58:09 +0530

    Added code for D2D students

--- CHARUSAT_result_scrapping.py
+++ CHARUSAT_result_scrapping.py
@@ -12,6 +12,10 @@ Year = 'NOVEMBER 2022'
 BASE_USERNAME = '20CS'
 start = 1
 end = 10
+D2D_BASE_USERNAME = 'D21CS'
+d2d_start = 1
+d2d_end = 1
+
 
 driver = webdriver.Firefox()
 driver.get("https://charusat.edu.in:912/UniExamResult/")
@@ -42,6 +46,27 @@ for i in range(start,end+1):
         driver.back()
     except:
         continue
+
+
+for i in range(d2d_start,d2d_end+1):
+    try:
+        s_id = D2D_BASE_USERNAME + str(i).zfill(3)
+        s_data[s_id] = list()
+        roll_field = driver.find_element(By.ID,"txtEnrNo")
+        driver.find_element(By.ID,"txtEnrNo").clear()
+        roll_field.send_keys(s_id)
+        submit_btn = driver.find_element(By.XPATH, '/html/body/form/div[3]/table[2]/tbody/tr[6]/td[1]/input')
+        submit_btn.click()
+        
+        s_data[s_id].append(driver.find_element(By.XPATH, '/html/body/form/div[3]/div/div/table[3]/tbody/tr[1]/td[6]/span').text)
+        s_data[s_id].append(driver.find_element(By.XPATH, '/html/body/form/div[3]/div/div/table[3]/tbody/tr[1]/td[3]/span').text)
+        s_data[s_id].append(driver.find_element(By.XPATH, '/html/body/form/div[3]/div/div/table[4]/tbody/tr[2]/td[3]/span').text)
+        s_data[s_id].append(driver.find_element(By.XPATH, '/html/body/form/div[3]/div/div/table[5]/tbody/tr[2]/td[3]/span').text)
+        driver.implicitly_wait(15)
+        driver.back()
+    except:
+        continue
+
 
 driver.close()
 exit()
